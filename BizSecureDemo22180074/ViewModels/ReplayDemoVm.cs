96d30ef4fee4e14c954b58466b0c433ab1c7fc4a
Valentin <valentinkiryanski@gmail.com>
2026-03-17 21:04:20 +0200

    excersise 6 - replay attack prevention

---
+++ BizSecureDemo22180074/ViewModels/ReplayDemoVm.cs
@@ -0,0 +1,13 @@
+﻿namespace BizSecureDemo22180074.ViewModels
+{
+    public class ReplayDemoVm
+    {
+
+        public decimal Balance { get; set; }
+        public string? Message { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; } = 100;
+        public string Token { get; set; } = "SECRET123";
+        public string Nonce { get; set; } = string.Empty;
+    }
+}
